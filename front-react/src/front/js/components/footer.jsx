8302dd8b840b9e37695944654d9870f31812f5a5
Marian Suárez <marianvsf@gmail.com>
2025-06-18 01:47:37 +0000

    footer

--- front-react/src/front/js/components/footer.jsx
+++ front-react/src/front/js/components/footer.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-// import marianvsfLogo from "../../assets/logo.jpg"
+import marianvsfLogo from "../../../assets/logo2.jpg"
 
 const Footer = () => {
   return(
 	<footer className="footer mt-auto py-3 text-center fixed-bottom" bg="dark" data-bs-theme="dark" 
   			style={{ color: 'grey'}} > 
 			<p>Made by 
-			<a href="https://github.com/Marianvsf"> Marian Suarez✨</a>
+			<a href="https://github.com/Marianvsf"> Marian Suarez
+			<img className="logo"
+              src={marianvsfLogo}
+              style={{
+                border: "1px solid rgb(255, 255, 255)",
+                borderRadius: "50%",
+                width: "35px",
+              }}
+              alt="logo"
+            />
+✨</a>
 			
 		</p>
 	</footer>
